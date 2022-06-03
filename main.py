7a170f15142f9bf1cdbc8344e25a09d00f1f6ad6
LuZhengy-e <ddlzy@hnu.edu.cn>
2022-06-03 23:38:55 +0800

    more clean version

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 from deployment.utils.maps import LoaclMap
 from matplotlib import pyplot as plt
 
+from config.LuZhengye import params
+
 logger = loguru.logger
 rad = np.pi / 180
 
@@ -26,33 +28,14 @@ def main():
     cfg = ConfigParser()
     cfg.read(args.config_path)
 
-    # read sensor info
-    HFOV = float(cfg.get("MAP", "HFOV")) * rad
-    VFOV = float(cfg.get("MAP", "VFOV")) * rad
-    fx = float(cfg.get("MAP", "fx"))
-    fy = float(cfg.get("MAP", "fy"))
-
     # construct optimize problem
     map_path = cfg.get("MAP", "map_path")
     local_map = LoaclMap(map_path)
 
     # optimize deployment each road
-    params = dict(
-        map=local_map,
-        Hs=np.arange(3, 5, 1),
-        phis=np.arange(0, np.pi / 2, 15 * rad),
-        gap_pole=50,
-        gap_road=2.5,
-        cameras_info=dict(
-            HFOV=HFOV,
-            VFOV=VFOV,
-            fx=fx,
-            k=0.05217551,
-            delta_x=1.75,
-            prob_thresh=0.6
-        )
-    )
-    build_from_dp("deployment_lu", **params)
+    params["map"] = local_map
+    method = params.pop("method")
+    build_from_dp(method, **params)
 
     osm_path = cfg.get("FILE", "osm_path")
     fig_path = cfg.get("FILE", "fig_path")
